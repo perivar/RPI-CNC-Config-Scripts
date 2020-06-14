8be2c23bdf259292e162c3cd716d410647974ba5
Per Ivar Nerseth <perivar@nerseth.com>
2020-06-14 20:32:10 +0200

    Added some comments

--- scripts/010-install-easel.sh
+++ scripts/010-install-easel.sh
@@ -3,3 +3,8 @@
 
 # use the fork where the nvm part has been updated. The? forces curl not never use cache
 curl "https://raw.githubusercontent.com/perivar/easel-driver/master/easel-driver.sh?$(date +%s)" | sh -x
+
+# the script above starts the driver in the backround using
+# screen -L -dmS easel node iris.js &&
+
+# Run `screen -r easel` to access the driver, and Ctrl+A (Cmd+A on macOS) followed by 'd' to detach)
